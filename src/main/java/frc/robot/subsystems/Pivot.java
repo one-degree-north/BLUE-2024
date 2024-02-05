48f2d14989260852bd6a2c814cecaf0999fa1645
lim44799 <lim44799@sas.edu.sg>
2024-02-05 17:14:11 +0800

    added follow pivot motor

--- src/main/java/frc/robot/subsystems/Pivot.java
+++ src/main/java/frc/robot/subsystems/Pivot.java
@@ -13,29 +13,33 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Pivot extends SubsystemBase {
   /** Creates a new Pivot. */
 
-  private CANSparkMax m_pivot;
-  private SparkPIDController m_pivotPidController;
+  private CANSparkMax m_pivotLead;
+  private CANSparkMax m_pivotFollow;
+  private SparkPIDController m_pivotPIDController;
   private RelativeEncoder m_pivotEncoder;
   public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
 
   public Pivot() {
 
-    m_pivot = new CANSparkMax(0, MotorType.kBrushless);
+    m_pivotLead = new CANSparkMax(0, MotorType.kBrushless);
+    m_pivotFollow = new CANSparkMax(0, MotorType.kBrushless);
 
   }
 
   private void configMotors(){
 
+    m_pivotFollow.follow(m_pivotLead);
+
     //Neo 550 Conifg
-    m_pivot.restoreFactoryDefaults();
-    m_pivot.setSmartCurrentLimit(20);
-    m_pivot.setInverted(false);
-    m_pivot.setOpenLoopRampRate(0);
-    m_pivot.setIdleMode(CANSparkMax.IdleMode.kCoast);
+    m_pivotLead.restoreFactoryDefaults();
+    m_pivotLead.setSmartCurrentLimit(20);
+    m_pivotLead.setInverted(false);
+    m_pivotLead.setOpenLoopRampRate(0);
+    m_pivotLead.setIdleMode(CANSparkMax.IdleMode.kCoast);
 
     //Get PID Controller and Encoder
-    m_pivotPidController = m_pivot.getPIDController();
-    m_pivotEncoder = m_pivot.getEncoder();
+    m_pivotPIDController = m_pivotLead.getPIDController();
+    m_pivotEncoder = m_pivotLead.getEncoder();
 
     //PID Controller Config
     kP = 0; 
@@ -46,18 +50,17 @@ public class Pivot extends SubsystemBase {
     kMaxOutput = 0; 
     kMinOutput = 0;
 
-    m_pivotPidController.setP(kP);
-    m_pivotPidController.setI(kI);
-    m_pivotPidController.setD(kD);
-    m_pivotPidController.setIZone(kIz);
-    m_pivotPidController.setFF(kFF);
-    m_pivotPidController.setOutputRange(kMinOutput, kMaxOutput);
-
+    m_pivotPIDController.setP(kP);
+    m_pivotPIDController.setI(kI);
+    m_pivotPIDController.setD(kD);
+    m_pivotPIDController.setIZone(kIz);
+    m_pivotPIDController.setFF(kFF);
+    m_pivotPIDController.setOutputRange(kMinOutput, kMaxOutput);
 
   }
 
   public void setPivot(double setpoint){
-    m_pivotPidController.setReference(setpoint, CANSparkMax.ControlType.kPosition);
+    m_pivotPIDController.setReference(setpoint, CANSparkMax.ControlType.kPosition);
   }
 
   public double getMotorPos(){
